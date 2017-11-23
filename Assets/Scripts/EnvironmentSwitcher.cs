c9a81f9d327e3e15687f94e7b709a7799957bdc1
msro1 <msro1@student.monash.edu>
2017-11-23 20:18:21 +1100

    Organisation and Environment work
    > Relabled most things in the assets so that they are easier to find in
    the search menu
    > Moved assets around so that they are in 'smarter' locations
    > Renamed 'Tablewear' to be 'Props' and moved most prefabs across to
    that
    > Started working on a new environment
    > Added a non-default skybox to the game
    > Started working on the environment switcher

---
+++ Assets/Scripts/EnvironmentSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSwitcher : MonoBehaviour {
+	//Array of environment prefabs
+	public GameObject[] Environments;
+
+	//The offset for spawning
+	private Vector3 Offset = new Vector3(3.0f, -0.5f, 5.0f);
+
+	//Current envrionment that is spawnned
+	private int CurrentEnvironment;
+
+
+	// Use this for initialization
+	void Start () {
+		CreateParams();
+
+		//Spawns the selected environment at the right location
+		GameObject SpawnedEnvironment = GameObject.Instantiate(Environments[CurrentEnvironment], Offset, Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//For setting up all the parameters
+	void CreateParams() {
+		//Picks a random environment to be spawned
+		CurrentEnvironment = Random.Range(0, Environments.Length);
+	}
+}
