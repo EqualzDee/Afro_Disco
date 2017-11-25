d286d6e93ec110140db8430c5254f9950012630e
msro1 <msro1@student.monash.edu>
2017-11-25 19:57:31 +1100

    Environment (cont.)
    > Fixed walking animations
    > Added walking NPCs that walk around the streets (I need to add a
    spawning/destroying thing, not sure how yet)
    > Added sliding doors in front of buildings that animate open/closed
    when NPCs walk passed
    > Consolidated all buttons in the menus into a single prefab for easier
    manipulation (Will redo all game UI soon)

---
+++ Assets/Scripts/NPCWalker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCWalker : MonoBehaviour {
+
+	public float WalkSpeedMin = 0.5f;
+	public float WalkSpeedMax = 1.5f;
+	private float WalkSpeed;
+
+	private int WalkCycle;
+
+	private Vector3 WalkDirection = Vector3.forward;
+	private int WalkingSpeedHash = Animator.StringToHash("WalkingSpeed");
+	private int BlendHash = Animator.StringToHash("Blend");
+
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponentInChildren<Animator>();
+		WalkSpeed = Random.Range(WalkSpeedMin, WalkSpeedMax);
+		myAnimator.SetFloat(WalkingSpeedHash, WalkSpeed);
+
+		WalkCycle = Random.Range(0, 2);
+		myAnimator.SetFloat(BlendHash, WalkCycle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(WalkDirection * Time.deltaTime * WalkSpeed);
+	}
+}
---
+++ Assets/Scripts/SlidingDoorTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlidingDoorTrigger:MonoBehaviour {
+
+	public BoxCollider myTrigger;
+	private Animator myAnimator;
+	private int OpenHash = Animator.StringToHash("OpenDoor");
+
+	public bool EnableOpening = true;
+
+	// Use this for initialization
+	void Start() {
+		myAnimator = GetComponentInChildren<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(EnableOpening) {
+			myAnimator.SetBool(OpenHash, true);
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if(EnableOpening) {
+			myAnimator.SetBool(OpenHash, false);
+		}
+	}
+}
